0cc6af5bca46b0c6732c2ac1a870164e252c6ed7
popa.catalin <popa.catalin@moldtelecom.md>
2024-11-01 15:22:21 +0200

    Fix salt save to db

--- MyCloudDomain/Auth/CreateUserLoginEntitiy.cs
+++ MyCloudDomain/Auth/CreateUserLoginEntitiy.cs
@@ -8,12 +8,12 @@ namespace MyCloudDomain.Auth
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
-        public byte[] Salt { get; set; }
+        public string Salt { get; set; }
         public DateTime CreateDate { get; } = DateTime.Now;
 
         public bool CheckPassword(string password)
         {
-            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, Salt, 350000, HashAlgorithmName.SHA512, 32);
+            var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, Convert.FromHexString(Salt), 350000, HashAlgorithmName.SHA512, 32);
             return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(Password));
         }
     }
